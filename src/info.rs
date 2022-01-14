7df400ea612093353960d4483c9ec657ff51e78a
RubixDev <silas.groh@t-online.de>
2022-01-14 20:57:37 +0100

    Initial Commit

---
+++ src/info.rs
@@ -0,0 +1,274 @@
+use std::{io::Read, process::Command, env, path::Path};
+use chrono::Duration;
+use systemstat::{System as StatSystem, Platform};
+use sysinfo::{System as InfoSystem, SystemExt, ProcessExt, ProcessorExt};
+
+pub struct System {
+    systemstat: StatSystem,
+    sysinfo: InfoSystem,
+}
+
+impl System {
+    pub fn new() -> System {
+        let mut s = System {
+            systemstat: StatSystem::new(),
+            sysinfo: InfoSystem::new(),
+        };
+        s.sysinfo.refresh_memory();
+        s.sysinfo.refresh_processes();
+        return s;
+    }
+
+    fn is_android(&self) -> bool {
+        Path::new("/system/app").exists() && Path::new("/system/priv-app").exists()
+    }
+
+    pub fn user_at_hostname(&self) -> Option<String> {
+        let user = env::var("USER");
+        if let Ok(user) = user {
+            return Some(format!("{}@{}", user, self.sysinfo.host_name()?));
+        } else {
+            return Some(format!(
+                "{}@{}",
+                String::from_utf8_lossy(&Command::new("id").arg("-un").output().ok()?.stdout).replace('\n', ""),
+                self.sysinfo.host_name()?,
+            ));
+        }
+    }
+
+    pub fn os(&self) -> Option<String> {
+        let version = self.sysinfo.os_version();
+        if let Some(version) = version {
+            if self.is_android() {
+                return Some(format!("Android {}", version));
+            }
+            if !version.contains("rolling") {
+                return Some(format!("{} {}", self.sysinfo.name()?, version));
+            }
+        }
+        return self.sysinfo.name();
+    }
+
+    pub fn host(&self) -> Option<String> {
+        if self.is_android() {
+            return self.sysinfo.name();
+        }
+
+        let name_file = std::fs::File::open("/sys/devices/virtual/dmi/id/product_name").ok();
+        let version_file = std::fs::File::open("/sys/devices/virtual/dmi/id/product_version").ok();
+        let model_file = std::fs::File::open("/sys/firmware/devicetree/base/model").ok();
+
+        if [&name_file, &version_file, &model_file].iter().all(|f| f.is_none()) { return None }
+
+        let name = if let Some(mut file) = name_file {
+            let mut str = String::new();
+            file.read_to_string(&mut str).unwrap_or(0);
+            str.replace('\n', "")
+        } else { "".to_string() };
+        let version = if let Some(mut file) = version_file {
+            let mut str = String::new();
+            file.read_to_string(&mut str).unwrap_or(0);
+            str.replace('\n', "")
+        } else { "".to_string() };
+        let model = if let Some(mut file) = model_file {
+            let mut str = String::new();
+            file.read_to_string(&mut str).unwrap_or(0);
+            str.replace('\n', "")
+        } else { "".to_string() };
+
+        let host = format!("{} {} {}", name, version, model);
+        let blacklist = [
+            "To",
+            "to",
+            "Be",
+            "be",
+            "Filled",
+            "filled",
+            "By",
+            "by",
+            "O.E.M.",
+            "OEM",
+            "Not",
+            "Applicable",
+            "Specified",
+            "System",
+            "Product",
+            "Name",
+            "Version",
+            "Undefined",
+            "Default",
+            "string",
+            "INVALID",
+            "os",
+            "Type1ProductConfigId",
+        ];
+        let mut host_filtered: Vec<String> = vec![];
+        for word in host.split(' ') {
+            if !blacklist.contains(&word) {
+                host_filtered.push(word.to_owned())
+            }
+        }
+        let host_n = host_filtered.join(" ");
+        if host_n.is_empty() {
+            return Some(
+                String::from_utf8_lossy(
+                    &Command::new("uname")
+                        .arg("-m")
+                        .output()
+                        .ok()?
+                        .stdout
+                    ).replace('\n', ""
+                )
+            );
+        } else {
+            return Some(host_n);
+        }
+    }
+
+    pub fn kernel(&self) -> Option<String> {
+        self.sysinfo.kernel_version()
+    }
+
+    pub fn uptime(&self) -> Option<String> {
+        let duration = Duration::from_std(self.systemstat.uptime().ok()?).ok()?;
+
+        let days = duration.num_days();
+        let hours = duration.num_hours() - 24 * days;
+        let minutes = duration.num_minutes() - 60 * hours - 24 * 60 * days;
+
+        Some(format!(
+            "{}{}{}m",
+            if days > 0 { format!("{}d ", days) } else { String::new() },
+            if hours > 0 { format!("{}h ", hours) } else { String::new() },
+            minutes
+        ))
+    }
+
+    pub fn packages(&self) -> Option<String> {
+        let commands = vec![
+            Command::new("pacman").arg("-Qq").output(),
+            Command::new("dpkg-query").args(["-f", r".\n", "-W"]).output(),
+            Command::new("bonsai").arg("list").output(),
+            Command::new("pkginfo").arg("-i").output(),
+            Command::new("rpm").arg("-qa").output(),
+            Command::new("xbps-query").arg("-l").output(),
+            Command::new("apk").arg("info").output(),
+            Command::new("guix").args(["package", "--list-installed"]).output(),
+            Command::new("opkg").arg("list-installed").output(),
+            Command::new("kiss").arg("l").output(),
+            Command::new("cpt-list").output(),
+            Command::new("pacman-g2").arg("-Q").output(),
+            Command::new("lvu").arg("installed").output(),
+            Command::new("tce-status").arg("-i").output(),
+            Command::new("pkg_info").output(),
+            Command::new("pkgin").arg("list").output(),
+            Command::new("gaze").arg("installed").output(),
+            Command::new("alps").arg("showinstalled").output(),
+            Command::new("butch").arg("list").output(),
+            Command::new("swupd").args(["bundle-list", "--quiet"]).output(),
+            Command::new("pisi").arg("li").output(),
+            Command::new("pacstall").arg("-L").output(),
+            // TODO: emerge
+        ];
+
+        for command in commands {
+            if let Ok(output) = command {
+                if !output.status.success() { continue; }
+                return Some(
+                    String::from_utf8_lossy(&output.stdout)
+                        .trim_matches('\n')
+                        .split('\n')
+                        .count()
+                        .to_string()
+                );
+            }
+        }
+        return None;
+    }
+
+    pub fn shell(&self) -> Option<String> {
+        Some(env::var("SHELL").ok()?.rsplit_once('/')?.1.to_owned())
+    }
+
+    pub fn terminal(&self) -> Option<String> {
+        let home = env::var("HOME");
+        if let Ok(home) = home {
+            if home.contains("termux") {
+                return Some(String::from("termux"));
+            }
+        }
+
+        let process = self.sysinfo.process(std::process::id() as i32)?;
+        let shell = self.sysinfo.process(process.parent()?)?;
+        let terminal = self.sysinfo.process(shell.parent()?)?;
+
+        let mut name = terminal.name();
+        if name == "electron" {
+            let parent_pid = terminal.parent();
+            if let Some(pid) = parent_pid {
+                let parent = self.sysinfo.process(pid);
+                if let Some(parent) = parent {
+                    let p_parent_pid = parent.parent();
+                    if let Some(p_pid) = p_parent_pid {
+                        let p_parent = self.sysinfo.process(p_pid);
+                        if let Some(p_parent) = p_parent {
+                            if parent.name().contains("code") || p_parent.name().contains("code") {
+                                name = "vscode"
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return Some(name.to_owned());
+    }
+
+    pub fn cpu(&self) -> Option<String> {
+        Some(self.sysinfo.processors().iter().next()?.brand().to_owned())
+    }
+
+    pub fn memory(&self) -> Option<String> {
+        Some(format!(
+            "{:.2}GB / {:.2}GB",
+            (self.sysinfo.used_memory()  as f32) / 1024.0 / 1024.0,
+            (self.sysinfo.total_memory() as f32) / 1024.0 / 1024.0,
+        ))
+    }
+
+    pub fn swap(&self) -> Option<String> {
+        Some(format!(
+            "{:.2}GB / {:.2}GB",
+            (self.sysinfo.used_swap()  as f32) / 1024.0 / 1024.0,
+            (self.sysinfo.total_swap() as f32) / 1024.0 / 1024.0,
+        ))
+    }
+
+    pub fn battery(&self) -> Option<String> {
+        let battery = self.systemstat.battery_life().ok()?;
+        let on_ac = self.systemstat.on_ac_power().ok()?;
+
+        let remaining_time = Duration::from_std(battery.remaining_time).ok()?;
+
+        let days = remaining_time.num_days();
+        let hours = remaining_time.num_hours() - 24 * days;
+        let minutes = remaining_time.num_minutes() - 60 * hours - 24 * 60 * days;
+
+        Some(format!(
+            "{}%, {}{}{}m remaining, {}charging",
+            (battery.remaining_capacity * 100.0).round()/*  as i32 */,
+            if days > 0 { format!("{}d ", days) } else { String::new() },
+            if hours > 0 { format!("{}h ", hours) } else { String::new() },
+            minutes,
+            if on_ac { "" } else { "dis" }
+        ))
+    }
+
+    pub fn colors1(&self) -> String {
+        (0..8).map(|c| format!("\x1b[4{}m   ", c)).collect::<String>() + "\x1b[0m"
+    }
+
+    pub fn colors2(&self) -> String {
+        (8..16).map(|c| format!("\x1b[48;5;{}m   ", c)).collect::<String>() + "\x1b[0m"
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,76 @@
+use std::path::PathBuf;
+
+// user@hostname
+//
+// OS
+// Host
+// kernel
+// uptime
+// packages
+// shell
+// terminal
+// cpu
+// memory
+// battery
+//
+// colors
+
+mod info;
+
+fn main() {
+    let sys = info::System::new();
+
+    let info: Vec<(&str, String)> = [
+        ("u@h",      sys.user_at_hostname()),
+        ("OS",       sys.os()),
+        ("Host",     sys.host()),
+        ("Kernel",   sys.kernel()),
+        ("Uptime",   sys.uptime()),
+        ("Packages", sys.packages()),
+        ("Shell",    sys.shell()),
+        ("Terminal", sys.terminal()),
+        ("CPU",      sys.cpu()),
+        ("Memory",   sys.memory()),
+        ("Swap",     sys.swap()),
+        ("Battery",  sys.battery()),
+        ("",         Some(String::new())),
+        ("colors",   Some(sys.colors1())),
+        ("colors",   Some(sys.colors2())),
+    ].iter().filter(|(_, v)| v != &None).map(|(k, v)| (*k, v.to_owned().unwrap())).collect();
+
+    let col: u8;
+    let img_filename = match info.iter().find(|e| e.0 == "OS") {
+        Some(distro) => {
+            let distro = distro.1.as_str();
+
+            if distro == "Arch Linux" { col = 4; "logos/arch.png" }
+            else if distro.contains("Android") { col = 2; "logos/android.png" }
+            else if distro.contains("Debian") { col = 1; "logos/debian.png" }
+            else { col = 3; "logos/tux.png" }
+        },
+        None => { col = 3; "logos/tux.png" },
+    };
+
+    let img_width = 30;
+    let img_str = ansipix::of_image(PathBuf::from(img_filename), (img_width, 64), 50, false).expect("error");
+    let img: Vec<&str> = img_str.trim_matches('\n').split('\n').collect();
+
+    for line in 0..(img.len().max(info.len())) {
+        if line < img.len() {
+            print!("{}  ", img[line]);
+        } else {
+            print!("{}  ", " ".repeat(img_width));
+        }
+
+        if line < info.len() {
+            if info[line].0 == "u@h" {
+                print!("\x1b[1;3{}m{}\x1b[0m", col + 1, info[line].1);
+            } else if info[line].0 == "colors" {
+                print!("{}", info[line].1)
+            } else if !info[line].0.is_empty() {
+                print!("\x1b[1;3{}m{: <9}\x1b[0m {}", col, info[line].0.to_owned() + ":", info[line].1);
+            }
+        }
+        println!();
+    }
+}
